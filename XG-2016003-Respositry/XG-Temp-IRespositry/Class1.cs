68e6cf62412bad0b364b5a47c2fef4199d53fc5c
xg <583477919@qq.com>
2016-01-14 10:56:12 +0800

    首次提交
    主要是自己维护的一个.net基础框架,之后会将学习的一些新东西 做成demo 慢慢完善。

---
+++ XG-2016003-Respositry/XG-Temp-IRespositry/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XG_Temp_IRespositry
+{
+    public class Class1
+    {
+    }
+}
---
+++ XG-2016003-Respositry/XG.Temp.RespositryMSSQL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XG.Temp.RespositryMSSQL
+{
+    public class Class1
+    {
+    }
+}
